7898bc3b0dc81ec378f1eaf8e47b8d8b6e390033
vietanh <dragonboynro67k@gmail.com>
2023-02-06 17:23:27 +0700

    Add new project food

--- comments.js
+++ comments.js
@@ -1,32 +1,4 @@
 // ----------------------------------------------------------------------
-const bell = document.querySelector('.fa-bell')
-const backgroundBell = document.querySelector('.background-bell')
-function Bell(){
-    backgroundBell.classList.toggle('open')
-}
-bell.addEventListener('click', Bell)
-
-const search = document.querySelector('.fa-magnifying-glass')
-const backgroundSearch = document.querySelector('#input')
-function Search(){
-    backgroundSearch.classList.toggle('open')
-}
-search.addEventListener('click', Search)
-
-const subnav = document.querySelector('#more')
-const backgroundSubnav = document.querySelector('#subnav')
-function Subnav(){
-    backgroundSubnav.classList.toggle('open')
-}
-subnav.addEventListener('click', Subnav)
-
-const avatar = document.querySelector('.avatar')
-const boxInfomation = document.querySelector('.box-infomation')
-function Avatar(){
-    boxInfomation.classList.toggle('open')
-}
-avatar.addEventListener('click', Avatar)    
-
 const reels = document.querySelector('#reels')
 const tin = document.querySelector('#tin')
 const meeting = document.querySelector('#phonghopmat')
@@ -133,9 +105,3 @@ function OpenMeetingOpen(){
 }
 meetingOpen.addEventListener('click', OpenMeetingOpen)
 
-const inputPosts = document.querySelector('#input-posts')
-const comments = document.querySelector('.fb-comments')
-function OpenComments(){
-    comments.classList.toggle('open')
-}
-inputPosts.addEventListener('click', OpenComments)
